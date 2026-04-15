62ffe6d0b2966879a7598d9d7f2551b4fea7bc8f
JoshuaGB <joshuagodinez1818@gmail.com>
2026-04-15 09:44:14 -0600

    Avance 5

--- Administrador.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-start "" /min cmd /c ".\mvnw.cmd javafx:run -Djavafx.args=admistrador"
---
+++ src/main/java/cr/ac/una/tarea/Main.java
@@ -0,0 +1,8 @@
+
+package cr.ac.una.tarea;
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
